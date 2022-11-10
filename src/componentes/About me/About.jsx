91620d1f62a57bbc1732d2a7f0bebda6103940e9
FacundoQuiroga9 <facu.quirogaga@gmail.com>
2022-11-10 12:33:16 -0300

    otros cambios

--- src/componentes/About me/About.jsx
+++ src/componentes/About me/About.jsx
@@ -6,7 +6,7 @@ const AboutMe= ()=>{
     <section className="aboutMe-contenedor" id='about-me'>
       <div className="aboutMe-info-contenedor">
         <h3 className="aboutMe-info-titulo">About <span>m</span>e</h3>
-        <p className="aboutMe-info-texto">Cuando comencé a programar descubrí un mundo con un montón de posibilidades para explorar mis conocimientos y creatividad. Desde entonces, me encuentro envuelto en un aprendizaje constante permitiendome perfeccionarme como desarrollador web.</p>
+        <p className="aboutMe-info-texto">Cuando comencé a programar descubrí un mundo con un montón de posibilidades para explorar mis conocimientos y creatividad. Desde entonces, me encuentro envuelto en un aprendizaje constante permitiendo perfeccionarme como desarrollador web.</p>
       </div>
       <div className="aboutMe-foto-contenedor">
         <img src={fotofq2} alt="Foto de Facundo" className="aboutMe-foto-foto" />
--- src/componentes/Projectss/Projects.jsx
+++ src/componentes/Projectss/Projects.jsx
@@ -12,11 +12,11 @@ const Projects = ()=>{
     <div id='projects'>
     <h3 className="section-projects">Projec<span>t</span>s</h3>
     <div className="projects-contenedor">
-      <Card imagen= {image2} nombre="Reproductor de música" tecnologias="HTML, CSS y Javascript"/>
-      <Card imagen= {image3} nombre="Reproductor de video" tecnologias="HTML, CSS y Javascript"/>
-      <Card imagen= {image4} nombre="Calculadora" tecnologias="HTML, CSS, Javascript y React"/>
-      <Card imagen= {image5} nombre="Anotador de tareas" tecnologias="HTML, CSS, Javascript y React"/>
-      <Card imagen= {image1} nombre="Buscador de letras" tecnologias="HTML, CSS y Javascript"/>
+      <Card imagen= {image2} nombre="Reproductor de música" tecnologias="HTML, CSS y Javascript" url="https://github.com/FacundoQuiroga9/Reproductor-de-musica" url2="https://facundoquiroga9.github.io/Reproductor-de-musica/"/>
+      <Card imagen= {image3} nombre="Reproductor de video" tecnologias="HTML, CSS y Javascript" url="https://github.com/FacundoQuiroga9/Reproductor-de-video" url2="https://facundoquiroga9.github.io/Reproductor-de-video/"/>
+      <Card imagen= {image4} nombre="Calculadora" tecnologias="HTML, CSS, Javascript y React" url="https://github.com/FacundoQuiroga9/Calculadora" url2="https://facundoquiroga9.github.io/Calculadora/"/>
+      <Card imagen= {image5} nombre="Anotador de tareas" tecnologias="HTML, CSS, Javascript y React" url="https://github.com/FacundoQuiroga9/Anotador-de-tareas" url2="https://facundoquiroga9.github.io/Anotador-de-tareas/"/>
+      <Card imagen= {image1} nombre="Buscador de letras" tecnologias="HTML, CSS y Javascript" url="https://github.com/FacundoQuiroga9/Buscador-de-letras" url2="https://facundoquiroga9.github.io/Buscador-de-letras/"/>
     </div>
     </div>
   )
