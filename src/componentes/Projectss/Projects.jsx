a45ddd8b47c263e4afea399ca9ebf2a41a698e33
Facundo Quiroga <facu.quirogaga@gmail.com>
2023-12-21 13:21:02 -0600

    Agregados los logos de las tecnologias en las cards de los proyectos

--- src/componentes/Projectss/Projects.jsx
+++ src/componentes/Projectss/Projects.jsx
@@ -2,10 +2,16 @@ import './Projects.css'
 import Card from '../Card/Card'
 import image1 from '../../imagenes/Fixi.png'
 import image2 from '../../imagenes/Ffarm Taid.png'
-import image3 from '../../imagenes/Reproductor de video.jpg'
-import image4 from '../../imagenes/calculadora.jpg'
-import image5 from '../../imagenes/tareas.jpg'
+import image3 from '../../imagenes/Dynamite fireworks.jpg'
 import {useTranslation} from 'react-i18next'
+import html from '../../imagenes/html.png'
+import css from '../../imagenes/css.png'
+import bootstrap from '../../imagenes/bootstrap.png'
+import postgresql from '../../imagenes/postgresql.png'
+import ruby from '../../imagenes/ruby.png'
+import ror from '../../imagenes/ror.png'
+import react from '../../imagenes/react.png'
+import js from '../../imagenes/js.png'
 
 
 const Projects = ()=>{
@@ -14,11 +20,26 @@ const Projects = ()=>{
     <div id='projects'>
     <h3 className="section-projects">{t("projects.section")}</h3>
     <div className="projects-contenedor">
-    <Card imagen= {image1} nombre={t("projects.card5.title")} tecnologias={t("projects.card5.techs")} url="https://github.com/elisaherre/FIXI_SERVICE" url2="http://www.fixiservice.xyz/"/>
-      <Card imagen= {image2} nombre={t("projects.card1.title")} tecnologias={t("projects.card1.techs")} url="https://github.com/FacundoQuiroga9/Ffarm-Taid" url2="https://hosteriaffarmtaid.com/"/>
-      <Card imagen= {image3} nombre={t("projects.card2.title")} tecnologias={t("projects.card2.techs")} url="https://github.com/FacundoQuiroga9/Reproductor-de-video" url2="https://facundoquiroga9.github.io/Reproductor-de-video/"/>
-      <Card imagen= {image4} nombre={t("projects.card3.title")} tecnologias={t("projects.card3.techs")} url="https://github.com/FacundoQuiroga9/Calculadora" url2="https://facundoquiroga9.github.io/Calculadora/"/>
-      <Card imagen= {image5} nombre={t("projects.card4.title")} tecnologias={t("projects.card4.techs")} url="https://github.com/FacundoQuiroga9/Anotador-de-tareas" url2="https://facundoquiroga9.github.io/Anotador-de-tareas/"/>
+    <Card imagen= {image1} nombre={t("projects.card5.title")} tecnologias={t("projects.card5.techs")} url="https://github.com/elisaherre/FIXI_SERVICE" url2="http://www.fixiservice.xyz/">
+      <img className='logo-card' src={css} alt="" />
+      <img className='logo-card' src={html} alt="" />
+      <img className='logo-card' src={bootstrap} alt="" />
+      <img className='logo-card' src={postgresql} alt="" />
+      <img className='logo-card' src={ruby} alt="" />
+      <img className='logo-card' src={ror} alt="" />
+    </Card>
+      <Card imagen= {image2} nombre={t("projects.card1.title")} tecnologias={t("projects.card1.techs")} url="https://github.com/FacundoQuiroga9/Ffarm-Taid" url2="https://hosteriaffarmtaid.com/">
+        <img className='logo-card' src={html} alt="" />
+        <img className='logo-card' src={css} alt="" />
+        <img className='logo-card' src={js} alt="" />
+        <img className='logo-card' src={react} alt="" />
+      </Card>
+      <Card imagen= {image3} nombre={t("projects.card2.title")} tecnologias={t("projects.card2.techs")} url="https://github.com/FacundoQuiroga9/justice-fireworks" url2="https://dynamitefireworks.uno/">
+        <img className='logo-card' src={html} alt="" />
+        <img className='logo-card' src={css} alt="" />
+        <img className='logo-card' src={js} alt="" />
+        <img className='logo-card' src={react} alt="" />
+      </Card>
     </div>
     </div>
   )
