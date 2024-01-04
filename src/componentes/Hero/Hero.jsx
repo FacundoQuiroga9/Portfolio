b65abb5572ba461a957533be42c7a6ed0e328a49
Facundo Quiroga <facu.quirogaga@gmail.com>
2024-01-04 14:30:27 -0600

    Pasando a bootstrap

--- src/componentes/Hero/Hero.jsx
+++ src/componentes/Hero/Hero.jsx
@@ -1,8 +1,6 @@
 import './Hero.css'
 import fotofq1 from '../../imagenes/FacundoQuiroga2.png'
 import {useTranslation} from 'react-i18next'
-import { motion } from "framer-motion"
-import { useInView } from 'react-intersection-observer';
 
 
 const Hero = ()=>{
@@ -10,41 +8,21 @@ const Hero = ()=>{
 
   const lng = localStorage.getItem("lng") || "es"
 
-  const [ref, inView] = useInView({
-    triggerOnce: true,
-  });
-
-  const heroPhotoAnimation = {
-    hidden: {  x:-300, opacity:0},
-    visible: { x: 0, opacity: 1 },
-  };
-  const heroInfoAnimation = {
-    hidden: {  x:100, opacity:0},
-    visible: { x: 0, opacity: 1 },
-  };
   return(
-    <section id="section-hero" className="hero-contenedor section">
-      <motion.div className="hero-foto-contenedor"
-      ref={ref}
-      initial="hidden"
-      animate={inView ? 'visible' : 'hidden'}
-      variants={heroPhotoAnimation}
-      transition={{duration:1, delay:.5}}
-      >
+    <section id="section-hero" className="container d-flex align-items-center">
+      <div className="row align-items-center">
+      <div className="col-sm-12 col-md-5 col-lg-4 col-xl-4 px-5 px-md-2 px-lg-4">
         <img src={fotofq1} alt="Foto de Facundo" className='hero-foto' />
-      </motion.div>
-      <motion.div className='hero-info-contenedor'
-        ref={ref}
-        initial="hidden"
-        animate={inView ? 'visible' : 'hidden'}
-        variants={heroInfoAnimation}
-        transition={{duration:1, delay:.5}}
-      >
-        <h1 className="hero-info-nombre">Facundo Quiroga</h1>
-        <h2 className="hero-info-trabajo">{t("hero.job")}</h2>
-        <p className="hero-info-text">{t("hero.welcome")}</p>
-        <button className="hero-info-cv"><a href={`/Resume.pdf`} target='_blanck' download='Resume_Facundo_Quiroga.pdf'>{t("hero.button")}</a></button>
-      </motion.div>
+      </div>
+      <div className='col-sm-12 col-md-7 col-lg-8 col-xl-8 px-4 my-4 text-center'>
+        <h1 className="hero-title">Facundo Quiroga</h1>
+        <h2 className="hero-subtitle">{t("hero.job")}</h2>
+        <p className="hero-text">{t("hero.welcome")}</p>
+        <a href={`/Resume.pdf`} className='btn-custom mt-3 p-3' target='_blanck' download='Resume_Facundo_Quiroga.pdf'>{t("hero.button")}</a>
+      </div>
+      </div>
+
+
 
     </section>
   )
