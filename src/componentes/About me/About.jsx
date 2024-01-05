24b10541b7aaf2e3e1f05510b5f5394f97687804
Facundo Quiroga <facu.quirogaga@gmail.com>
2024-01-05 10:38:56 -0600

    Terminada seccion about

--- src/componentes/About me/About.jsx
+++ src/componentes/About me/About.jsx
@@ -1,7 +1,6 @@
 import './About.css'
 import ReactonRails from '../../imagenes/2h.png'
 import {useTranslation} from 'react-i18next'
-import { motion } from "framer-motion"
 import { useInView } from 'react-intersection-observer';
 
 const AboutMe= ()=>{
@@ -9,37 +8,21 @@ const AboutMe= ()=>{
 
   const [ref, inView] = useInView({
     triggerOnce: true,
+    threshold: 0.5,
   });
 
-  const aboutImageAnimation = {
-    hidden: {  x:-300, opacity:0},
-    visible: { x: 0, opacity: 1 },
-  };
-  const aboutInfoAnimation = {
-    hidden: {  x:100, opacity:0},
-    visible: { x: 0, opacity: 1 },
-  };
+
   return(
-    <section className="aboutMe-contenedor section" id='about-me'>
-      <motion.div className="aboutMe-foto-contenedor"
-      ref={ref}
-      initial="hidden"
-      animate={inView ? 'visible' : 'hidden'}
-      variants={aboutImageAnimation}
-      transition={{duration:1, delay:.5}}
-      >
-        <img src={ReactonRails} alt="Foto de Facundo" className="aboutMe-foto-foto" />
-      </motion.div>
-      <motion.div className="aboutMe-info-contenedor"
-      ref={ref}
-      initial="hidden"
-      animate={inView ? 'visible' : 'hidden'}
-      variants={aboutInfoAnimation}
-      transition={{duration:1, delay:.5}}
-      >
-        <h3 className="aboutMe-info-titulo">{t("about.section")}</h3>
-        <p className="aboutMe-info-texto">{t("about.text")}</p>
-      </motion.div>
+    <section ref={ref} className="container d-flex align-items-center" id='about-me'>
+      <div className="row">
+      <div className="col-12 col-md-6 col-lg-4 col-xl-4 px-5 px-md-0 d-flex align-items-center">
+        <img src={ReactonRails} alt="Foto de Facundo" className={`aboutMe-image animate__animated ${inView ? 'animate__backInUp animate__slow' : ''}`} />
+      </div>
+      <div className="col-12 col-md-6 col-lg-8 col-xl-8 px-5 px-md-2 px-lg-4">
+        <h3 className={`aboutMe-title animate__animated ${inView ? 'animate__rotateInDownLeft' : ''}`}>{t("about.section")}</h3>
+        <p className={`aboutMe-text animate__animated ${inView ? 'animate__fadeIn animate__slower' : ''}`}>{t("about.text")}</p>
+      </div>
+      </div>
     </section>
   )
 }
--- src/componentes/Hero/Hero.jsx
+++ src/componentes/Hero/Hero.jsx
@@ -11,18 +11,18 @@ const Hero = ()=>{
   const lng = localStorage.getItem("lng") || "es"
 
   const [ref, inView] = useInView({
-    triggerOnce: true, // La animación solo se activará una vez
-    threshold: 0.5, // Ajusta el umbral según tus necesidades
+    triggerOnce: true,
+    threshold: 0.5,
   });
 
   return(
-    <section id="section-hero" className="container d-flex align-items-center">
+    <section ref={ref} id="section-hero" className="container d-flex align-items-center">
       <div className="row align-items-center">
       <div className="col-sm-12 col-md-5 col-lg-4 col-xl-4 px-5 px-md-2 px-lg-4">
-        <img src={fotofq1} alt="Foto de Facundo" className={`hero-photo animate__animated ${inView ? 'animate__swing' : ''}`} />
+        <img src={fotofq1} alt="Foto de Facundo" className={`hero-photo animate__animated ${inView ? 'animate__swing animate__slow' : ''}`} />
       </div>
       <div className='col-sm-12 col-md-7 col-lg-8 col-xl-8 px-4 my-4 text-center'>
-        <h1 ref={ref} className={`hero-title animate__animated ${inView ? 'animate__rotateInDownLeft' : ''}`}>Facundo Quiroga</h1>
+        <h1 className={`hero-title animate__animated ${inView ? 'animate__rotateInDownLeft' : ''}`}>Facundo Quiroga</h1>
         <h2 className={`hero-subtitle animate__animated ${inView ? 'animate__fadeIn animate__slower' : ''}`}>{t("hero.job")}</h2>
         <p className={`hero-text animate__animated ${inView ? 'animate__fadeIn animate__slower' : ''}`}>{t("hero.welcome")}</p>
         <a href={`/Resume.pdf`} className={`btn-custom mt-3 p-3 animate__animated ${inView ? 'animate__pulse animate__faster' : ''}`} target='_blanck' download='Resume_Facundo_Quiroga.pdf'>{t("hero.button")}</a>
