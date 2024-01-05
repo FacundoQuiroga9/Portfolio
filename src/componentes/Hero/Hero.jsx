d4cf411c8985ad81ddefb744eac7eade7c4cdc1b
Facundo Quiroga <facu.quirogaga@gmail.com>
2024-01-05 09:05:02 -0600

    Terminado de animar el hero

--- src/componentes/Hero/Hero.jsx
+++ src/componentes/Hero/Hero.jsx
@@ -19,13 +19,13 @@ const Hero = ()=>{
     <section id="section-hero" className="container d-flex align-items-center">
       <div className="row align-items-center">
       <div className="col-sm-12 col-md-5 col-lg-4 col-xl-4 px-5 px-md-2 px-lg-4">
-        <img src={fotofq1} alt="Foto de Facundo" className='hero-foto' />
+        <img src={fotofq1} alt="Foto de Facundo" className={`hero-photo animate__animated ${inView ? 'animate__swing' : ''}`} />
       </div>
       <div className='col-sm-12 col-md-7 col-lg-8 col-xl-8 px-4 my-4 text-center'>
-        <h1 className="hero-title animate__animated animate__rotateInDownLeft">Facundo Quiroga</h1>
-        <h2 className="hero-subtitle">{t("hero.job")}</h2>
-        <p className="hero-text">{t("hero.welcome")}</p>
-        <a href={`/Resume.pdf`} className='btn-custom mt-3 p-3' target='_blanck' download='Resume_Facundo_Quiroga.pdf'>{t("hero.button")}</a>
+        <h1 ref={ref} className={`hero-title animate__animated ${inView ? 'animate__rotateInDownLeft' : ''}`}>Facundo Quiroga</h1>
+        <h2 className={`hero-subtitle animate__animated ${inView ? 'animate__fadeIn animate__slower' : ''}`}>{t("hero.job")}</h2>
+        <p className={`hero-text animate__animated ${inView ? 'animate__fadeIn animate__slower' : ''}`}>{t("hero.welcome")}</p>
+        <a href={`/Resume.pdf`} className={`btn-custom mt-3 p-3 animate__animated ${inView ? 'animate__pulse animate__faster' : ''}`} target='_blanck' download='Resume_Facundo_Quiroga.pdf'>{t("hero.button")}</a>
       </div>
       </div>
 
